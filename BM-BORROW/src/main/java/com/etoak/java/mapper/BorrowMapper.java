ffb816af1d6f5a42746a2f4298b4e022b5cc6f98
K1llerZ <107487300+K1llerMrZ@users.noreply.github.com>
2024-04-08 01:32:30 +0800

    Delete BM-BORROW directory

--- BM-BORROW/src/main/java/com/etoak/java/mapper/BorrowMapper.java
+++
@@ -1,9 +0,0 @@
-package com.etoak.java.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.etoak.java.entity.Borrow;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BorrowMapper extends BaseMapper<Borrow> {
-}
