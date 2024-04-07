f00326f0080f8d33276c9c0be89b8fd38f3130fb
K1llerZ <107487300+K1llerMrZ@users.noreply.github.com>
2024-04-08 01:32:44 +0800

    Delete BM-ORDER directory

--- BM-ORDER/src/main/java/com/etoak/java/mapper/OrderMapper.java
+++
@@ -1,9 +0,0 @@
-package com.etoak.java.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.etoak.java.entity.Order;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface OrderMapper extends BaseMapper<Order> {
-}
