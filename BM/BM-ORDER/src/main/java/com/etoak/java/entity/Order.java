674aa5848b12527c26611b07e42724a4722fd9d5
K1llerZ <1559600883@qq.com>
2024-04-08 02:14:46 +0800

    1111

--- BM/BM-ORDER/src/main/java/com/etoak/java/entity/Order.java
+++ BM/BM-ORDER/src/main/java/com/etoak/java/entity/Order.java
@@ -11,7 +11,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.math.BigDecimal;
 import java.util.Date;
 
-@TableName(value = "order")
+@TableName(value = "orders")
 @Data
 
 public class Order {
--- BM/BM-ORDER/src/main/java/com/etoak/java/service/IOrderService.java
+++ BM/BM-ORDER/src/main/java/com/etoak/java/service/IOrderService.java
@@ -3,6 +3,13 @@ package com.etoak.java.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.etoak.java.entity.Order;
 
-public interface IOrderService extends IService<Order> {
+import java.util.List;
 
+public interface IOrderService extends IService<Order> {
+    List<Order> get(Order order);
+
+    int add(Order order);
+    int delete(Integer id);
+    int update(Order order);
+    int examine(Integer id, Integer allow);
 }
