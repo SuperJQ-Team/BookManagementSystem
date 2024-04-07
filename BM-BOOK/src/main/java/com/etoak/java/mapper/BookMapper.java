850b9ce9d4f19da86bb94a3fa8b0771e2c683ebc
K1llerZ <107487300+K1llerMrZ@users.noreply.github.com>
2024-04-08 01:32:21 +0800

    Delete BM-BOOK directory

--- BM-BOOK/src/main/java/com/etoak/java/mapper/BookMapper.java
+++
@@ -1,9 +0,0 @@
-package com.etoak.java.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.etoak.java.entity.Book;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BookMapper extends BaseMapper<Book> {
-}
